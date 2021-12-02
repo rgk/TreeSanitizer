117f6ec949db8821228349c28474fbc120a994ee
Robert Kowalski <rgkmg25@gmail.com>
2021-12-02 05:19:38 -0500

    Update edge.js

--- tests/edge.js
+++ tests/edge.js
@@ -56,3 +56,36 @@ test('Convert values that are arrays into incrementing object properties.', (t) 
 
   t.end();
 });
+
+test('String object input is treated like an array.', (t) => {
+  let data = new String("thisisastring");
+
+  t.deepEqual(
+    new TreeSanitizer(data).run(),
+    { 0: 't', 1: 'h', 2: 'i', 3: 's', 4: 'i', 5: 's', 6: 'a', 7: 's', 8: 't', 9: 'r', 10: 'i', 11: 'n', 12: 'g' }
+  );
+
+  t.end();
+});
+
+test('Boolean object input returns a empty object.', (t) => {
+  let data = new Boolean(true);
+
+  t.deepEqual(
+    new TreeSanitizer(data).run(),
+    {}
+  );
+
+  t.end();
+});
+
+test('Number object input returns a empty object.', (t) => {
+  let data = new Number(13);
+
+  t.deepEqual(
+    new TreeSanitizer(data).run(),
+    {}
+  );
+
+  t.end();
+});
